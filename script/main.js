01f371d906a715be4158cd97b17fa80eb7aaa5e2
sherhard <goeasysher@gmail.com>
2023-08-03 21:19:50 +0800

    07 day of java script

---
+++ script/main.js
@@ -0,0 +1,9 @@
+function fullName() {
+    let name = 'Sher';
+    document.write(name)
+}
+fullName();
+function printFullName(firstName, lastName) {
+    return `${firstName} ${lastName}`
+}
+document.write(printFullName('Sher', 'Zhyldyzbek'))
